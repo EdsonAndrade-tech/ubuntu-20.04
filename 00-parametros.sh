a96ec9c148ea8a1a743afe926600c0f4377c008e
Edson Andrade <63322638+EdsonAndrade-tech@users.noreply.github.com>
2022-09-02 12:14:00 +0100

    Parâmetros (variáveis de ambiente)
    utilizados nos scripts de instalação dos Serviços

---
+++ 00-parametros.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Autor: Edson Andrade
+# Github: https://github.com/EdsonAndrade-tech
+# Data de criação: 02/09/2022
+# Data de atualização: 
+# Versão: 0.01
+# Ubuntu Server 20.04.x LTS x64 
