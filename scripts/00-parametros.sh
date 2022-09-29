0d15a303058081f1a6b159feb74549e349460cba
Edson Andrade <e.andrade666@gmail.com>
2022-09-29 09:18:41 +0100

    Configuracao  do OpenSSH Server e Alteracao de 13 arquivos

--- scripts/00-parametros.sh
+++ scripts/00-parametros.sh
@@ -2,8 +2,8 @@
 # Autor: Edson Andrade
 # Github: https://github.com/EdsonAndrade-tech
 # Data de criação: 02/09/2022
-# Data de atualização: 
-# Versão: 0.01
+# Data de atualização: 29/09/2022 
+# Versão: 0.02
 # Ubuntu Server 20.04.x LTS x64 
 #============================================================================================#
 
